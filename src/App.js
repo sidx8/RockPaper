93bdc2c5aeefe95ede15b7af3ab78833bcb21b57
siddharth <siddharth0sinha@gmail.com>
2021-07-20 00:14:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles/output.css";
+
+function App() {
+  return (
+    <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+      <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
+      <p className="text-gray-400 mt-5 text-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+        consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+        eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+      </p>
+      <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+        Hello Friends 🚀
+      </button>
+    </div>
+  );
+}
+
+export default App;
